749e4075cde83546a60587510ae52d6eb83581b4
Japmeet Singh <japmeet01@gmail.com>
2022-05-30 18:51:12 +0530

    Added Styled Components

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import ActorCard from './ActorCard';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../StyledComponents';
 
 const ActorGrid = ({ data }) => {
   return (
-    <div>
-      <div>
-        {data.map(({ person }) => (
-          <ActorCard
-            key={person.id}
-            id={person.id}
-            name={person.name}
-            gender={person.gender}
-            country={person.country ? person.country.name : null}
-            birthday={person.birthday}
-            deathday={person.deathday}
-            image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
-            summary={person.summary}
-          />
-        ))}
-      </div>
-    </div>
+    <FlexGrid>
+      {data.map(({ person }) => (
+        <ActorCard
+          key={person.id}
+          id={person.id}
+          name={person.name}
+          gender={person.gender}
+          country={person.country ? person.country.name : null}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+          summary={person.summary}
+        />
+      ))}
+    </FlexGrid>
   );
 };
 
