5c26c88e6577ec788118a05eb832925fcc102971
Japmeet Singh <japmeet01@gmail.com>
2022-05-31 22:43:03 +0530

    Styled show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,25 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { api_get } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  errors: null,
+};
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAIL': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
 
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errors, setErrors] = useState(null);
+  //   const [show, setShow] = useState(null);
+  //   const [isLoading, setIsLoading] = useState(true);
+  //   const [errors, setErrors] = useState(null);
+
+  const [{ show, isLoading, errors }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   useEffect(() => {
     let isMounted = true;
     api_get(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(result => {
         if (isMounted) {
-          setShow(result);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', show: result });
+          //   setShow(result);
+          //   setIsLoading(false);
         }
       })
       .catch(err => {
         if (isMounted) {
-          setErrors(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAIL', error: err.message }); //dispatch(action,)
+          //   setErrors(err.message);
+          //   setIsLoading(false);
         }
       });
 
@@ -37,7 +69,33 @@ const Show = () => {
     return <div>Error Occured:{errors}</div>;
   }
 
-  return <div>Show Page</div>;
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 };
 
 export default Show;
